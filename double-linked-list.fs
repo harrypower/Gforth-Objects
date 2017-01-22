34a9efb1713be692a27bec8806d70dc83dffc902
Philip Smith <philipkingsmith@gmail.com>
2017-01-22 13:25:01 -0700

    added construct? test and node current adjusters

--- double-linked-list.fs
+++ double-linked-list.fs
@@ -25,6 +25,7 @@ require ./objects.fs
 object class
   destruction implementation
   selector ll@
+  selector construct?
   cell% inst-var size-link
   cell% inst-var first-link
   cell% inst-var last-link
@@ -37,9 +38,11 @@ object class
     cell% field payload-size  \ note node payload data size limit to cell quantity
     cell% field node-payload
   end-struct link-links%
+  m: ( -- nflag ) \ nflag is true if linked list is constructed false if not constructed yet!
+    dll-test? dll-test? @ = ;m overrides construct?
   public
   m: ( -- ) \ constructor
-    dll-test? dll-test? @ <> if
+    this construct? false = if
       0 size-link !
       0 first-link !
       0 last-link !
@@ -51,7 +54,7 @@ object class
     then
   ;m overrides construct
   m: ( -- ) \ destructor
-    dll-test? dll-test? @ = if
+    this construct? if
       0 size-link @ = if exitm then  \ nothing to deallocate
       \ *** code to deallocate through the linked list nodes here ***
     then
@@ -61,8 +64,8 @@ object class
     first-link @ u. ." start node's address" cr
     last-link @ u. ." the last node's address" cr
     current-link @ u. ." current node's address" cr
-    dll-test? @ dll-test? = if ." construct done!" cr else ." construct not done!" cr then
-    size-link @ 0 > dll-test? @ dll-test? = and true =
+    this construct? if ." construct done!" cr else ." construct not done!" cr then
+    size-link @ 0 > this construct? and true =
     if
       current-link @ next-back-link @ u. ." current node's back link address" cr
       current-link @ next-forward-link @ u. ." current node's forward link address" cr
@@ -73,32 +76,77 @@ object class
   m: { caddr u -- } \ add to link list a node at the end and update all the link list node data
     \ caddr  is address of data to add to this node
     \ u is the quantity of bytes to add to this node
-    size-link @ 0 = if
-      link-links% %size u + allocate throw
-      dup first-link !
-      dup last-link !
-      dup current-link !
-      dup dup next-back-link !
-      dup dup next-forward-link !
-      dup caddr swap node-payload u move
-      payload-size u swap !
-      1 size-link !
-    else
-      link-links% %size u + allocate throw
-      dup last-link @ next-forward-link !
-      dup last-link @ swap next-back-link !
-      dup last-link !
-      dup caddr swap node-payload u move
-      payload-size u swap !
-      size-link @ 1+ size-link !
+    this construct? if
+      size-link @ 0 = if
+        link-links% %size u + allocate throw
+        dup first-link !
+        dup last-link !
+        dup current-link !
+        dup dup next-back-link !
+        dup dup next-forward-link !
+        dup caddr swap node-payload u move
+        payload-size u swap !
+        1 size-link !
+      else
+        link-links% %size u + allocate throw
+        dup last-link @ next-forward-link !
+        dup last-link @ swap next-back-link !
+        dup last-link !
+        dup caddr swap node-payload u move
+        payload-size u swap !
+        size-link @ 1+ size-link !
+      then
     then
   ;m method ll!
   m: ( -- caddr u ) \ get node data from current node
-    size-link @ 0 <>
+    \ if there is no nodes in the linked list u will be 0 and caddr will be 0 indicating a null retrieval
+    this construct? size-link @ 0 > and
     if
       current-link @ dup node-payload swap payload-size @
     else
       0 0 \ return null data if there are no nodes in this linked list
     then
   ;m overrides ll@
+  m: ( -- u ) \ get linked list node size
+    size-link @
+  ;m method ll-size@
+  m: ( -- nflag ) \ step one node back from current node.
+    \ nflag is true if step can not happend because at start node already or if there is no nodes in linked list to move to!
+    \ nflag is false if step did happen!
+    this construct? size-link @ 0 > current-link @ first-link @ <> and and if
+      current-link @ next-back-link @ current-link ! false
+    else
+      true
+    then ;m method ll<
+  m: ( -- nflag ) \ step one node forward from current node.
+    \ nflag is true if step can not happen because at last node already or if there is no nodes in linked list to move to!
+    \ nflag is false if step did happen!
+    this construct? size-link @ 0 > current-link @ last-link @ <> and and if
+      current-link @ next-forward-link @ current-link ! false
+    else
+      true
+    then ;m method ll>
+  m: ( -- caddr u nflag ) \ get node payload and step to next node
+    \ nflag is true if step can not happen because at last node already or if there is no nodes in linked list to step to!
+    \ nflag is false if step did happen
+    \ caddr and u will will be the node payload before the step if there are any linked list nodes
+    \ if there are no linked list nodes caddr and u will both be 0
+    this ll@ this ll> ;m method ll@>
+  m: ( -- caddr u nflag ) \ get node payload and step to next node
+    \ nflag is true if step can not happen because at first node already or if there is no nodes in linked list to step to!
+    \ nflag is false if step did happen
+    \ caddr and u will will be the node payload before the step if there are any linked list nodes
+    \ if there are no linked list nodes caddr and u will both be 0
+    this ll@ this ll< ;m method ll@<
 end-class double-linked-list
+
+double-linked-list heap-new value lltest
+lltest print
+s" hello world" lltest ll!
+s" next line" lltest ll!
+s" line 3" lltest ll!
+lltest print
+lltest ll@> . cr type cr
+lltest ll@> . cr type cr
+lltest ll@> . cr type cr
+lltest ll-size@ . cr
