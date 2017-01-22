d535ef41dde21f2243ce10ff7f0be5b372e4c298
Philip Smith <philipkingsmith@gmail.com>
2017-01-21 17:55:13 -0700

    starting to work through the issues

--- double-linked-list.fs
+++ double-linked-list.fs
@@ -23,5 +23,35 @@ require ./objects.fs
 [endif]
 
 object class
-
+  destruction implementation
+  cell% inst-var size-link
+  cell% inst-var first-link
+  cell% inst-var last-link
+  cell% inst-var current-link
+  cell% inst-var dll-test?
+  protected
+  struct
+    cell% field next-forward-link
+    cell% field next-back-link
+  end-struct link-links
+  public
+  m: ( -- ) \ constructor
+    dll-test? dll-test? @ <> if
+      0 size-link !
+      0 first-link !
+      0 last-link !
+      0 current-link !
+      dll-test? dll-test? ! \ ensure future calls know constructor has allocated stuff
+    else
+      this destruct
+      0 dll-test? ! \ dll memory is freed so reset the test!
+    then
+  ;m overrides construct
+  m: ( -- ) \ destructor
+    dll-test? dll-test? @ = if
+      0 size-link @ = if 0 dll-test? ! exitm then  \ nothing to deallocate
+    then
+  ;m overrides destruct
+  m: ( -- ) \ print info
+  ;m overrides print
 end-class double-linked-list
